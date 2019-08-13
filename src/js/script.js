1c4b7a63ea635a28ff95365b4cb47cce682dfa0f
sakuma-kyoko <sakuma-kyoko@KYCNT1704-18.local>
2019-08-13 17:22:46 +0900

    回転機能とライト、マウス座標取得

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,10 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 camera.position.set(0, 1, 5);
 const grid   = new THREE.GridHelper(10, 5);
 
+const light = new THREE.DirectionalLight(0xFFFFFF);
+light.intensity = 1; // 光の強さを倍に
+light.position.set(1, 1, 1);
+
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
@@ -22,12 +26,52 @@ loader.load(url, (data) => {
     scene.add(object);
 });
 
-scene.add(grid, cube);
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.userPan = false;
+controls.userPanSpeed = 0.0;
+controls.maxDistance = 5000.0;
+controls.maxPolarAngle = Math.PI * 0.495;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1.0;
+
+//マウスのグローバル変数
+const mouse = { x: 0, y: 0 };  
+//オブジェクト格納グローバル変数
+const targetList = []; 
+//マウスが押された時
+window.onmousedown = function (ev){
+    if (ev.target == renderer.domElement) {     
+        //マウス座標2D変換
+        const rect = ev.target.getBoundingClientRect();    
+        mouse.x =  ev.clientX - rect.left;
+        mouse.y =  ev.clientY - rect.top;
+        //マウス座標3D変換 width（横）やheight（縦）は画面サイズ
+        mouse.x =  (mouse.x / window.innerWidth) * 2 - 1;           
+        mouse.y = -(mouse.y / window.innerHeight) * 2 + 1;
+        // マウスベクトル
+        const vector = new THREE.Vector3( mouse.x, mouse.y ,1);
+       // vector はスクリーン座標系なので, オブジェクトの座標系に変換
+        projector.unprojectVector( vector, camera );
+        // 始点, 向きベクトルを渡してレイを作成
+        const ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+         // クリック判定
+        const obj = ray.intersectObjects( targetList );
+         // クリックしていたら、alertを表示  
+        if ( obj.length > 0 ){                       
+          alert("click!!")
+       }
+    }
+   };
+
+scene.add(grid, cube, light);
+targetList.push(cube);
 
 const loop = () => {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render( scene, camera );
+    renderer.gammaOutput = true;
+    controls.update();
     window.requestAnimationFrame( loop ); 
 }
 window.requestAnimationFrame( loop );
