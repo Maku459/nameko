4a12ea3bfb20b7f47962c430e627aa795ccf1a5d
sakuma-kyoko <sakuma-kyoko@KYCNT1704-18.local>
2019-08-15 11:50:42 +0900

    収穫まで

--- src/js/script.js
+++ src/js/script.js
@@ -9,14 +9,20 @@ renderer.gammaOutput = true;
 
 const scene = new THREE.Scene();
 
+//カメラ
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0, 2, 10);
+camera.position.set(0, 4, 10);
 
+//地面・座標軸
 const gg = new THREE.PlaneBufferGeometry( 100, 100 );
 const gm = new THREE.MeshPhongMaterial( { color: 0x7b5544 } );
 const ground = new THREE.Mesh( gg, gm );
 ground.rotation.x = - Math.PI / 2;
 ground.position.y = -0.5;
+
+const axis = new THREE.AxisHelper(1000);
+axis.position.set(0,0,0);
+scene.add(axis);
 
 const light  = new THREE.AmbientLight(0xffffff, 1);
 
@@ -31,18 +37,24 @@ loader.load(url, (data) => {
 
         const gltf = data;
         const nameko = gltf.scene.clone();
-        group.position.x = Math.random() * 10 - 4;
+        group.position.x = _.random(-7.0, 7.0);
         group.position.y = 0.2;
-        group.position.z = Math.random() * 10 - 4;
-        group.rotation.y = Math.random() * 2 * Math.PI;
+        group.position.z = _.random(-3.5, 3.5);
+        group.rotation.y = _.random(0, 30);
+        group.interactive = true;
         nameko.scale.x = 0.5;
         nameko.scale.y = 0.5;
         nameko.scale.z = 0.5;
         nameko.name = 'nameko';
         group.add(nameko);
 
-        const geometry = new THREE.BoxGeometry(0.8,2.0,0.8);
-        const material = new THREE.MeshLambertMaterial({color: 0xE9546B, transparent: true, opacity: 0.1});
+        const geometry = new THREE.BoxGeometry(0.7,2.0,0.7);
+        const material = new THREE.MeshLambertMaterial(
+            {
+                color: 0xE9546B, 
+                transparent: true, 
+                opacity: 0.5
+            });
         const hitcube = new THREE.Mesh( geometry, material );
         hitcube.position.set(nameko.position.x, nameko.position.y + 0.3, nameko.position.z);
         hitcube.name = 'hitcube';
@@ -54,10 +66,11 @@ loader.load(url, (data) => {
         .map ((value, key, array) => value.getObjectByName('hitcube'))
         .compact()
         .value()
-    
-    console.log(hitcubes);
 });
 
+// function generateNameko(){}
+
+/* === 回転付加 === */
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.userPan = false;
 controls.userPanSpeed = 0.0;
@@ -66,11 +79,12 @@ controls.maxPolarAngle = Math.PI * 0.495;
 controls.autoRotate = false;
 controls.autoRotateSpeed = 1.0;
 
+/* === レイキャスティング === */
 const raycaster = new THREE.Raycaster();
 // マウス座標管理用のベクトルを作成
 const mouse = new THREE.Vector2();
 
-function onClick( event ) {
+function mouseMove( event ) {
     event.preventDefault();
 
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -79,28 +93,39 @@ function onClick( event ) {
     intersect();
 }
 
-console.log(scene.children);
-
 function intersect() {
 	// レイキャスト = マウス位置からまっすぐに伸びる光線ベクトルを生成
 	raycaster.setFromCamera( mouse, camera );
 
 	// その光線とぶつかったオブジェクトを得る
     const intersects = raycaster.intersectObjects(hitcubes);
-    console.log(intersects[0]);
 
-    const intersect_nameko = intersects[0].object.parent.getObjectByName('nameko');
-    console.log(intersect_nameko);
+    const intersect_nameko = intersects[0].object.parent;
+
+    if(!intersect_nameko.interactive) return;
+
+    intersect_nameko.interactive = false;
 
     // ぶつかったオブジェクトに対してなんかする
     if ( intersects.length > 0 ) {
-        intersect_nameko.scale.y = 1.0;
+        TweenMax.to(intersect_nameko.scale, 0.5, {y: 1.0, ease: Power1.easeIn});
+        TweenMax.to(intersect_nameko.position, 0.5, {y: 0.8, ease: Power1.easeIn});
+        TweenMax.to(intersect_nameko.scale, 0.1, {y: 0.2, delay: 0.5});
+        TweenMax.to(intersect_nameko.position, 0.1, {y: 1.2, delay: 0.5});
+        TweenMax.to(intersect_nameko.scale, 0.2, {y: 1.0, delay: 0.6});
+        TweenMax.to(intersect_nameko.position, 0.5, {
+            y: 10, 
+            delay: 1.1,
+            onComplete: () => {
+                console.log('complete');
+            }
+        });
     }
 }
 
-window.addEventListener( 'mousemove', onClick, false );
+window.addEventListener( 'mousemove', mouseMove, false );
 
-scene.add(light);
+scene.add(light, ground);
 
 const loop = () => {
     renderer.render( scene, camera );
@@ -108,5 +133,3 @@ const loop = () => {
     window.requestAnimationFrame( loop ); 
 }
 window.requestAnimationFrame( loop );
-
-console.log(scene.children);
